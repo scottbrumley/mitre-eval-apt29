7772422bda028d341a58217fdb15b836fc43034c
Scott Brumley <scott@nsisecure.com>
2023-12-12 20:13:12 +0000

    Updating Windows 2019 packer template

--- build-base.sh
+++ build-base.sh
@@ -11,29 +11,24 @@
 ##
 
 base_config_dir="./packer/base-config"
-# source config-build.sh
+source config-build
 
 export PACKER_LOG=1
 export PACKER_LOG_PATH="z-packer.log"
 
-echo $WIN2019_KEY
-echo $WIN10_KEY
-
 # Add Product Keys
 # Windows 2019 Key
-#cp -f "$base_config_dir/windows2019/autounattend-template.xml" "$base_config_dir/windows2019/autounattend.xml"
-#sed -i -e "s/\*\*ProductKey\*\*/$WIN2019_KEY/g" "$base_config_dir/windows2019/autounattend.xml"
-#echo $WIN2019_KEY
+cp -f "$base_config_dir/windows2019/autounattend-template.xml" "$base_config_dir/windows2019/autounattend.xml"
+sed -i -e "s/\*\*ProductKey\*\*/$win2019_key/g" "$base_config_dir/windows2019/autounattend.xml"
 
 # Windows 10 Key
-#cp -f "$base_config_dir/windows10/autounattend-template.xml" "$base_config_dir/windows10/autounattend.xml"
-#sed -i -e "s/\*\*ProductKey\*\*/$WIN10_KEY/g" "$base_config_dir/windows10/autounattend.xml"
-#echo $WIN10_KEY
+cp -f "$base_config_dir/windows10/autounattend-template.xml" "$base_config_dir/windows10/autounattend.xml"
+sed -i -e "s/\*\*ProductKey\*\*/$win10_key/g" "$base_config_dir/windows10/autounattend.xml"
 
-#packer build -force packer/ubuntu-server-22-04.json 
-#packer build -force packer/windows-10-22H2.json
-#packer build -force packer/windows-server-2019.json
+packer build -force packer/ubuntu-server-22-04.json 
+packer build -force packer/windows-10-22H2.json
+packer build -force packer/windows-server-2019.json
 
 # Remove Autounattend.xml files with Secrets
-# rm -rf "${base_config_dir}/windows2019/autounattend.xml"
-# rm -rf "${base_config_dir}/windows10/autounattend.xml"
+rm -rf "${base_config_dir}/windows2019/autounattend.xml"
+rm -rf "${base_config_dir}/windows10/autounattend.xml"
